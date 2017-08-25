832dedd7ac6802eeae364ff74dd8b7de14e58af6
Margaret Berry <margaretshelaghmcgovern@gmail.com>
2017-08-25 15:34:02 -0700

    add html and js to show individual details for mulitple pizzas

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 //back end
-  //constructors
+//variables
+var userToppings =[];
+// var totalOrder = '';
+//constructors
 function Pizza (size, toppings) {
   this.size = size;
   this.toppings = toppings;
   this.basePrice = 17;
 }
-  //methods
+//methods
 Pizza.prototype.cost = function() {
   if (this.size === "small") {
     var mySizePrice = this.basePrice - 5;
@@ -29,20 +32,51 @@ Pizza.prototype.cost = function() {
   return myFinalPrice;
 }
 
+// function findTotal () {
+//   for (i = 0; i <= userPizzas.length; i++) {
+//     var totalOrder = userPizzas += userPizzas[i];
+//   }
+// }
+
 //front end
 $(document).ready(function() {
   $('#choosePizza').submit(function(event) {
     event.preventDefault();
-    var userToppings =[];
     var userPizzas = [];
+//get user input
     var userSize = $('input[name=size]:checked').val();
     $('input[name=topping]:checked').each(function() {
       var userTopping = $(this).val();
       userToppings.push(userTopping);
     });
+//use user input to make new pizza
     var myPizza = new Pizza (userSize, userToppings);
-    $('ul#userPizzas').append("<li><span>" + myPizza.size + "  with " + myPizza.toppings + "</span></li>");
+    userPizzas.push(parseInt(myPizza.cost()));
+//show order summary
+    $('ul#userPizzas').append("<li class='clickable'><span>My " + myPizza.size + " pizza</span></li>");
     $('#pizzaCost').text(myPizza.cost());
     $('#order').show();
+    $('#choosePizza').hide();
+//see pizza details
+    $('.clickable').last().click(function() {
+      $('#show-order').show();
+      $('.onePizza').text('Size - ' + myPizza.size + ', Cost - $' + (myPizza.cost()) + '.00');
+    });
+//add another pizza
+    $('#morePizza').click(function() {
+      $('#order').hide();
+      $('#show-order').hide();
+      $('#choosePizza').show();
+      $('#choosePizza')[0].reset();
+      alert(userPizzas);
+      alert(totalOrder);
+    });
+//hide pizza details
+    $('#hidePizza').click(function() {
+      $('#show-order').hide();
+      $('.onePizzaSize').text("");
+      $('.onePizzaToppings').text("");
+      $('.onePizzaCost').text("");
+    });
   });
 });
